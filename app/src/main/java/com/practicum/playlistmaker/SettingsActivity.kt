548b0517fd23cd1b2da93b83f65c84755ea31a48
novinnik <novinnik@yandex.ru>
2025-06-19 11:55:51 +0300

    Init commit #1 - 20250619

--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -2,20 +2,20 @@ package com.practicum.playlistmaker
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toolbar
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.appbar.MaterialToolbar
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
-        val backButton = findViewById<Button>(R.id.button_back)
-
-        backButton.setOnClickListener{finish()}
-
-
+        val backButtonToolbar = findViewById<MaterialToolbar>(R.id.toolbar_activity_setting)
+        backButtonToolbar.setNavigationOnClickListener{finish()}
     }
+
 }
