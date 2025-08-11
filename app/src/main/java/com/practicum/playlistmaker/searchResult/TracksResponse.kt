826606693dac444fc8d77c914005f8760057ff01
novinnik <novinnik@yandex.ru>
2025-08-11 15:44:01 +0300

    Init commit #7 - Задача спринта 12 20250811

--- app/src/main/java/com/practicum/playlistmaker/searchResult/TracksResponse.kt
+++ app/src/main/java/com/practicum/playlistmaker/searchResult/TracksResponse.kt
@@ -3,8 +3,6 @@ package com.practicum.playlistmaker.searchResult
 import com.google.gson.annotations.SerializedName
 
 class TracksResponse (
-    @SerializedName("results") val results : ArrayList<Track>,
+    @SerializedName("results") val results : MutableList<Track>,
     @SerializedName("resultCount") val resultCount : Int
-//    val results : ArrayList<Track>,
-//   val resultCount : Int
 )
